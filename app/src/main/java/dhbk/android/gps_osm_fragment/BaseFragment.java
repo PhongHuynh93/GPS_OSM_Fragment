07e04763599b39f65f0eae05455252cc72802f8b
Phong <p7157568@gmail.com>
2016-04-23 00:10:31 +0700

    feat - make map + base fragment

--- app/src/main/java/dhbk/android/gps_osm_fragment/BaseFragment.java
+++ app/src/main/java/dhbk/android/gps_osm_fragment/BaseFragment.java
@@ -1,10 +1,50 @@
 package dhbk.android.gps_osm_fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 /**
  * Created by huynhducthanhphong on 4/22/16.
  */
-public class BaseFragment extends Fragment{
+public abstract class BaseFragment extends Fragment {
+    private final int mLayout;
+    private MapView mMapView;
+    private View mRootView;
+    private IMapController mIMapController;
 
+    public BaseFragment(int layout) {
+        this.mLayout = layout;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mRootView = inflater.inflate(mLayout, container, false);
+        return mRootView;
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        makeMapDefaultSetting();
+    }
+
+    public void makeMapDefaultSetting() {
+        mMapView = (MapView) mRootView.findViewById(R.id.map); // map
+        mMapView.setTileSource(TileSourceFactory.MAPNIK);
+        mMapView.setMultiTouchControls(true);
+        mIMapController = mMapView.getController(); // map controller
+        mIMapController.setZoom(Constant.ZOOM);
+        GeoPoint startPoint = new GeoPoint(10.772241, 106.657676);
+        mIMapController.setCenter(startPoint);
+    }
 }
