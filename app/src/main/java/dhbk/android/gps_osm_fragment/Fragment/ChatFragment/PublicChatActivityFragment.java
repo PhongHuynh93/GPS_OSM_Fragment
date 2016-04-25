58c987a49d2ab556214e6595d36969bd3a08fb0a
Phong <p7157568@gmail.com>
2016-04-25 22:00:10 +0700

    feat -

--- app/src/main/java/dhbk/android/gps_osm_fragment/Fragment/ChatFragment/PublicChatActivityFragment.java
+++ app/src/main/java/dhbk/android/gps_osm_fragment/Fragment/ChatFragment/PublicChatActivityFragment.java
@@ -1,28 +1,39 @@
 package dhbk.android.gps_osm_fragment.Fragment.ChatFragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.ArrayList;
+
+import dhbk.android.gps_osm_fragment.Help.Chat;
+import dhbk.android.gps_osm_fragment.Help.ChatAdapter;
+import dhbk.android.gps_osm_fragment.Help.Config;
 import dhbk.android.gps_osm_fragment.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PublicChatActivityFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PublicChatActivityFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
+// TODO: 4/25/16 check layout xem chạy được ko
 public class PublicChatActivityFragment extends Fragment {
     private static final String ARG_NICK = "nick";
 
     private String mNick;
-
+    private ArrayList<Chat> mChatList;
+    private String mIdDevice;
+    private Firebase mRefFB;
+    private RecyclerView recyclerView;
+    private ChatAdapter chatAdapter;
 
     public PublicChatActivityFragment() {
     }
@@ -41,9 +52,14 @@ public class PublicChatActivityFragment extends Fragment {
         if (getArguments() != null) {
             mNick = getArguments().getString(ARG_NICK);
         }
+        Config.getFirebaseInitialize(getActivity());
+        // di vao phong chat public
+        mRefFB = Config.getFirebaseReference().child(Config.FIREBASE_CHILD_PUBLIC_CHAT);
+        mChatList = new ArrayList<>();
+        mIdDevice = Settings.Secure.getString(getActivity().getContentResolver(), Settings.Secure.ANDROID_ID);
+
     }
 
-    // TODO: 4/25/16 make layout recycler view
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -53,10 +69,68 @@ public class PublicChatActivityFragment extends Fragment {
 
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        chatAdapter = new ChatAdapter(mChatList, mIdDevice);
+        recyclerView = (RecyclerView) getActivity().findViewById(R.id.recycler_view_chat);
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        recyclerView.setAdapter(chatAdapter);
+
+        getActivity().findViewById(R.id.button_sent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMessageToSent();
+            }
+        });
+        getChatMessages();
 
     }
 
+    private void getMessageToSent(){
+        EditText editText = (EditText) getActivity().findViewById(R.id.edit_txt_message);
+        String message = editText.getText().toString();
+        if(!message.isEmpty())
+            mRefFB.push().setValue(new Chat(message, mIdDevice));
+        editText.setText("");
+    }
 
+    private void getChatMessages(){
+
+        mRefFB.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                if (dataSnapshot != null && dataSnapshot.getValue() != null) {
+
+                    //Firebase - Convierte una respuesta en un objeto de tipo Chat
+                    Chat model = dataSnapshot.getValue(Chat.class);
+                    mChatList.add(model);
+                    recyclerView.scrollToPosition(mChatList.size() - 1);
+                    chatAdapter.notifyItemInserted(mChatList.size() - 1);
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                firebaseError.getMessage();
+            }
+        });
+    }
 }
--- app/src/main/java/dhbk/android/gps_osm_fragment/Help/Config.java
+++ app/src/main/java/dhbk/android/gps_osm_fragment/Help/Config.java
@@ -8,10 +8,8 @@ import com.firebase.client.Firebase;
  * Created by Jhordan on 24/07/15.
  */
 public class Config {
-
-    // TODO: change this to your own Firebase URL
-    private static final String FIREBASE_URL = "https://test-fb-phong093.firebaseio.com";
-    private static final String FIREBASE_CHILD = "chat";
+    public static final String FIREBASE_URL = "https://test-fb-phong093.firebaseio.com";
+    public static final String FIREBASE_CHILD_PUBLIC_CHAT = "public-chat";
 
     public static void getFirebaseInitialize(Context context) {
         Firebase.setAndroidContext(context);
