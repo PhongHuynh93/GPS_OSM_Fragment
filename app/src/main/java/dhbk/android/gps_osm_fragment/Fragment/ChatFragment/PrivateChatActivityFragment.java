72a33ea0a0c762ea089e084a5d7e10c30a8f1e67
Phong <p7157568@gmail.com>
2016-04-26 14:54:30 +0700

    feat - hiện friend list

--- app/src/main/java/dhbk/android/gps_osm_fragment/Fragment/ChatFragment/PrivateChatActivityFragment.java
+++ app/src/main/java/dhbk/android/gps_osm_fragment/Fragment/ChatFragment/PrivateChatActivityFragment.java
@@ -2,14 +2,25 @@ package dhbk.android.gps_osm_fragment.Fragment.ChatFragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.firebase.client.Firebase;
+import com.firebase.ui.FirebaseListAdapter;
+
+import dhbk.android.gps_osm_fragment.Help.Config;
+import dhbk.android.gps_osm_fragment.Help.Nick;
 import dhbk.android.gps_osm_fragment.R;
 
+// TODO: 4/26/16 fix listview
 public class PrivateChatActivityFragment extends Fragment {
+    private FirebaseListAdapter<Nick> mAdapter;
+
     public PrivateChatActivityFragment() {
         // Required empty public constructor
     }
@@ -26,4 +37,22 @@ public class PrivateChatActivityFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_private_chat_activity, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        ListView messagesView = (ListView) getActivity().findViewById(R.id.list_private_chat);
+
+        Config.getFirebaseInitialize(getContext());
+        Firebase ref = Config.getFirebaseReference().child("nickList");
+
+        mAdapter = new FirebaseListAdapter<Nick>(getActivity(), Nick.class, android.R.layout.two_line_list_item, ref) {
+            @Override
+            protected void populateView(View view, Nick nick, int position) {
+                ((TextView)view.findViewById(android.R.id.text1)).setText(nick.getNick());
+            }
+        };
+        messagesView.setAdapter(mAdapter);
+    }
 }
---
+++ app/src/main/java/dhbk/android/gps_osm_fragment/Help/Nick.java
@@ -0,0 +1,20 @@
+package dhbk.android.gps_osm_fragment.Help;
+
+/**
+ * Created by huynhducthanhphong on 4/26/16.
+ */
+
+public class Nick {
+    private String nick;
+
+    public Nick() {
+    }
+
+    public Nick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+}
