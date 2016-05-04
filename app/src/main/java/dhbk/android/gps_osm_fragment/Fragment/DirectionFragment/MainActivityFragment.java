8098260f483fe62c0af8da3f8d32ae075f1d6368
Phong <p7157568@gmail.com>
2016-05-04 08:20:36 +0700

    feat

--- app/src/main/java/dhbk/android/gps_osm_fragment/Fragment/DirectionFragment/MainActivityFragment.java
+++ app/src/main/java/dhbk/android/gps_osm_fragment/Fragment/DirectionFragment/MainActivityFragment.java
@@ -59,6 +59,11 @@ public class MainActivityFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        BottomSheetFragment bottomSheetFragment = BottomSheetFragment.newInstance();
+        getChildFragmentManager()
+                .beginTransaction()
+                .add(R.id.map_bottom_sheets, bottomSheetFragment)
+                .commit();
         return rootView;
     }
 
